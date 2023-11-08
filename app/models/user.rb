a3f4f3908618966709b129fa44a2d493dc88e8b3
Kenn1476 <97831182+Kenn1476@users.noreply.github.com>
2023-11-08 21:50:12 +0900

    first commit

---
+++ app/models/user.rb
@@ -0,0 +1,11 @@
+class User < ApplicationRecord
+  validates :email, presence: true
+  validates :encrypted_password, presence: true
+
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+  has_many :prototypes
+  has_many :comments
+end
