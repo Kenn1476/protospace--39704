7c75e09ed76a551658e0f21c38e778c7a6d3f77e
Kenn1476 <97831182+Kenn1476@users.noreply.github.com>
2023-11-09 21:24:39 +0900

    final

--- db/migrate/20231104133419_create_comments.rb
+++ db/migrate/20231104133419_create_comments.rb
@@ -1,10 +1,9 @@
 class CreateComments < ActiveRecord::Migration[7.0]
   def change
     create_table :comments do |t|
-      t.integer :user_id
-      t.integer :prototype_id
       t.text :content
       t.references :user, null: false, foreign_key: true
+      t.references :prototype, null: false, foreign_key: true
       t.timestamps
     end
   end
