1160fb53fef253075dd42d3f129e86d3a76ed323
Kenn1476 <97831182+Kenn1476@users.noreply.github.com>
2023-11-11 19:05:15 +0900

    Correction

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -21,23 +21,22 @@ class PrototypesController < ApplicationController
   end
 
   def show
-    @prototype = Prototype.find(params[:id])
     @comment = Comment.new
     @comments = @prototype.comments.includes(:user)
   end
 
   def edit
-    @prototype = Prototype.find(params[:id])
   end
 
   def update
-    @prototype = Prototype.find(params[:id])
-    @prototype.update(prototype_params)
-    redirect_to root_path
+    if @prototype.update(prototype_params)
+      redirect_to prototype_path(@prototype)
+    else
+      render :edit, status: :unprocessable_entity
+    end
   end
 
   def destroy
-    @prototype = Prototype.find(params[:id])
     @prototype.destroy
     redirect_to root_path
   end
